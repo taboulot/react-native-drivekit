1b47f4ec884967bfd682568886df8e148f8a69f5
Clément Taboulot <clement-taboulot@hotmail.fr>
2022-12-06 11:52:02 +0100

    feat: generate new app that compile with new arch off

---
+++ example/babel.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: ['.tsx', '.ts', '.js', '.json'],
+        alias: {},
+      },
+    ],
+  ],
+};
---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  TestExample
+//
+
+import Foundation
